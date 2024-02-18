a595e9896cbcc05b79b1cd9ec46d65611e909047
heaur <143513292+heaur@users.noreply.github.com>
2024-02-18 17:13:15 +0100

    Initial commit

---
+++ src/main/java/com/example/oblig1data1700rktig/Oblig1data1700RktigApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700rktig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1data1700RktigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1data1700RktigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1data1700rktig/Oblig1data1700RktigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700rktig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1data1700RktigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
